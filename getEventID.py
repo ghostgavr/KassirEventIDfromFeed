62a156f8e9ea4fec0ee60ee29871412df5f780f6
Vladimir <31019740+ghostgavr@users.noreply.github.com>
2019-03-26 12:38:24 +0300

    Update getEventID.py

--- getEventID.py
+++ getEventID.py
@@ -21,13 +21,14 @@ print("loaded - ok")
 parsed = json.loads(data)
 
 
+
 with open('test.csv', 'w', newline="") as fileprint: #encoding='utf-8'
     writer = csv.writer(fileprint, delimiter=';', dialect='excel')
-    writer.writerow(["EventID", "Name", "Venue", "URL"])  # write header
+    writer.writerow(["EventID", "Name", "Venue", "VenueID", "URL"])  # write header
 
     for x in parsed:
 
         if parsed[x]['venue'] not in halldict:
-            writer.writerow([str(x), str(parsed[x]['name']), str('not avalible ') + str(parsed[x]['venue']), str(parsed[x]['url'])])
+            writer.writerow([str(x), str(parsed[x]['name']), str('not avalible '), str(parsed[x]['venue']), str(parsed[x]['url'])])
         else:
-            writer.writerow([str(x), str(parsed[x]['name']), halldict[parsed[x]['venue']], str(parsed[x]['url'])])
+            writer.writerow([str(x), str(parsed[x]['name']), halldict[parsed[x]['venue']], str(parsed[x]['venue']), str(parsed[x]['url'])])
